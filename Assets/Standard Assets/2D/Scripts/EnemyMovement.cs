44aaac93aa0b8493955de77170f10d8c8f753f7b
Ethan <ethanjosephbrown12@gmail.com>
2020-08-18 10:32:24 -0500

    player attack script and enemy prefab

---
+++ Assets/Standard Assets/2D/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    int MinDist = 5;
+    SpriteRenderer mySprRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if (Vector2.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.position += transform.forward * MoveSpeed * Vector2.Distance(transform.position, Player.position) * Time.deltaTime;
+
+            if (Vector2.Distance(transform.position, Player.position) <= MaxDist)
+            {
+
+
+            }
+
+        }
+    }
+}
+
+
