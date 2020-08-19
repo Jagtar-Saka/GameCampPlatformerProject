da7e5093cd1435ec2de1626c9e46ad317829fdd1
Ethan <ethanjosephbrown12@gmail.com>
2020-08-19 10:33:06 -0500

    waypoints Scripts

---
+++ Assets/Standard Assets/2D/Scripts/Waypoints.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoin : MonoBehaviour
+{
+     [SerializeField]
+    private float moveSpeed = 2f;
+
+    [SerializeField]
+    private Transform[] waypoints;
+
+    private int waypointIndex = 0;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, waypoints[waypointIndex].transform.position,
+                moveSpeed * Time.deltaTime);
+            if (transform.position == waypoints[waypointIndex].transform.position)
+            {
+                waypointIndex += 1;
+            }
+        }
+        else
+        {
+            waypointIndex = 0;
+        }
+    }
+    
+}
